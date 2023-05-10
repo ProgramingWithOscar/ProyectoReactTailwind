6b6251fbd8a728f291dfcf903181a2acd47ef960
Oscar Poveda <oscarpoveda679@gmail.com>
2023-05-10 15:05:12 -0500

    Se agrego el sidebar y los iconos del mismo

--- dashboard-store/src/App.jsx
+++ dashboard-store/src/App.jsx
@@ -1,8 +1,9 @@
+import Sidebar from "./components/shared/Sidebar"
+
 function App() {
   return (
-    <div className="bg-red-500">
-    hola
-    como
+    <div className="bg-[#262837] w-full min-h-screen">
+   <Sidebar></Sidebar>
     </div>
   )
 }
